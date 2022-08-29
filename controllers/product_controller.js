37b285dddc17385079d156f19a2048e971bba2f8
brendanburke <bfburke33@gmail.com>
2022-08-29 10:57:45 -0400

    update and delete routes working in postman, deploying to heroku to  begin frontend work

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const { route } = require('express/lib/application')
 const router = express.Router()
 const { product } = require('../models')
 
 router.get('/', async (req, res, next) => {
     try {
-        res.json(await req.product)
+        res.json(await product.find({}))
     } catch (err) {
         console.log(err)
     }
@@ -26,7 +27,27 @@ router.get('/:id', async (req, res, next) => {
     let id = req.params.id
 
     try {
-        res.json(await Product.find({}))
+        res.json(await product.findById({_id: id}))
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.put('/update/:id', async (req, res, next) => {
+    let id = req.params.id
+    
+    try {
+        res.json(await product.findByIdAndUpdate(id, req.body))
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.delete('/delete/:id', async (req, res, next) => {
+    let id = req.params.id
+
+    try {
+        res.json(await product.findByIdAndDelete(id))
     } catch (err) {
         console.log(err)
     }
