194086254f85dcb3fdca659cb09f9e73f79bf6bc
brendanburke <bfburke33@gmail.com>
2022-08-29 12:49:29 -0400

    working on cors error for deployed site

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { PORT = 4000, MONGODB_URI } = process.env;
 
 require('./config/db.connection')
 
-app.use(cors());
+app.use(cors('*'));
 app.use(morgan("dev")); 
 app.use(express.json()); 
 
